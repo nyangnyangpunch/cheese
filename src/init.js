7fd69c339c210da2b096651fc545de9869081059
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-17 01:59:39 +0900

    UPDATE: 서버 라우트 로깅 로직 구현

--- src/init.js
+++ src/init.js
@@ -1,9 +1,26 @@
+const { logger } = require('./util/logger')
+
 const express = require('express')
 const config = require('config')
 const path = require('path')
 
 // 서버 초기화 스크립트
 module.exports = app => {
+  // 서버 접속 로깅
+  app.use('*', (_, res, next) => {
+    const afterResponse = () => {
+      res.removeListener('finish', afterResponse)
+      res.removeListener('close', afterResponse)
+      logger.info(`${res.req.method} ${res.statusCode} - ${res.req.originalUrl}`)
+    }
+
+    // 리스너 등록
+    res.on('finish', afterResponse)
+    res.on('close', afterResponse)
+    next()
+  })
+
+
   /* 정적파일 경로 */
   app.use('/js', express.static(
     path.join(global.__root, config.get('static'), '/js')))

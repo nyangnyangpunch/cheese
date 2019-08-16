8435c4fc0af49ded784c1530d3943d63a063736a
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-17 02:08:53 +0900

    UPDATE: 해야할 일 주석 추가

--- src/api/init.js
+++ src/api/init.js
@@ -5,5 +5,7 @@ module.exports = app => {
   app.get(API_ENDPOINT + '/testApi', async (req, res) => {
     logger.debug('Recieved: ' + req.query.text)
     res.json({ name: 'catpunch' })
+
+    // TODO: 명령어 받고 서버측에서 커맨드 실행하기
   })
 }

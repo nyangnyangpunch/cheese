3546a1ab80c42312f4c88ed5e8f9a58a7d8255d7
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-17 00:40:45 +0900

    UPDATE: 메뉴 클릭 이벤트, 로딩 영역 추가

--- static/dev/index.js
+++ static/dev/index.js
@@ -1,1 +1,65 @@
-const a = () => console.log('a')
+const menuList = [
+  {
+    name: 'project'
+  },
+  {
+    name: 'setting'
+  },
+  {
+    name: 'help'
+  }
+]
+
+/* 좌측 메뉴 이벤트 설정 */
+const initMenu = () => {
+  menuList.forEach(({ name }) => {
+    $(`#menu_${name}`).click(function () {
+      $('.drawer-item').removeClass('active')
+      $(`#menu_${name}`).addClass('active')
+      loadContent(name)
+    })
+  })
+}
+
+/* 상단 Header 우측의 타이틀 변경 */
+const changeTitle = text => {
+  const title = text.substring(0, 1).toUpperCase() +
+                text.substring(1, text.length)
+  $('#header_title').text(title)
+}
+
+/* 해당 페이지 로드 후 #content에 표시 */
+const loadContent = name => {
+  contentLoading(true)
+  $('#content').load(`/${name}`, () => {
+    setTimeout(() => {
+      changeTitle(name)
+      contentLoading(false)
+    }, 1000)
+  })
+}
+
+/* 로딩영역 show/hide */
+const contentLoading = show => {
+  if (show) {
+    $('#loading').removeClass('hide')
+  } else {
+    $('#loading').addClass('hide')
+  }
+}
+
+/* Quit 메뉴 */
+const quit = () => {
+  alert('Quit')
+}
+
+
+
+/*========== load ========== */
+$(function () {
+  $('#menu_quit').click(quit)
+
+  // 페이지 첫 로드시 Project 메뉴 보이기
+  loadContent('project')
+  initMenu()
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,65 @@
 "use strict";
 
-var a = function a() {
-  return console.log('a');
+var menuList = [{
+  name: 'project'
+}, {
+  name: 'setting'
+}, {
+  name: 'help'
+}];
+/* 좌측 메뉴 이벤트 설정 */
+
+var initMenu = function initMenu() {
+  menuList.forEach(function (_ref) {
+    var name = _ref.name;
+    $("#menu_".concat(name)).click(function () {
+      $('.drawer-item').removeClass('active');
+      $("#menu_".concat(name)).addClass('active');
+      loadContent(name);
+    });
+  });
 };
+/* 상단 Header 우측의 타이틀 변경 */
+
+
+var changeTitle = function changeTitle(text) {
+  var title = text.substring(0, 1).toUpperCase() + text.substring(1, text.length);
+  $('#header_title').text(title);
+};
+/* 해당 페이지 로드 후 #content에 표시 */
+
+
+var loadContent = function loadContent(name) {
+  contentLoading(true);
+  $('#content').load("/".concat(name), function () {
+    setTimeout(function () {
+      changeTitle(name);
+      contentLoading(false);
+    }, 1000);
+  });
+};
+/* 로딩영역 show/hide */
+
+
+var contentLoading = function contentLoading(show) {
+  if (show) {
+    $('#loading').removeClass('hide');
+  } else {
+    $('#loading').addClass('hide');
+  }
+};
+/* Quit 메뉴 */
+
+
+var quit = function quit() {
+  alert('Quit');
+};
+/*========== load ========== */
+
+
+$(function () {
+  $('#menu_quit').click(quit); // 페이지 첫 로드시 Project 메뉴 보이기
+
+  loadContent('project');
+  initMenu();
+});

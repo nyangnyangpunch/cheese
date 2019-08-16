1c7600fa52b5da37f15fe232435f2ddf94d1266b
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-17 02:08:17 +0900

    UPDATE: Restful API 테스트 코드 추가 (정상 동작)

--- src/api/init.js
+++ src/api/init.js
@@ -1,7 +1,9 @@
+const { logger } = require('../util/logger')
 const API_ENDPOINT = '/API'
 
 module.exports = app => {
   app.get(API_ENDPOINT + '/testApi', async (req, res) => {
+    logger.debug('Recieved: ' + req.query.text)
     res.json({ name: 'catpunch' })
   })
 }
--- static/dev/help.js
+++ static/dev/help.js
@@ -5,5 +5,22 @@
  */
 
 $(function () {
-  
+
+  // API 테스트
+  $('#send').click(function () {
+    $.ajax({
+      url: '/api/testApi',
+      method: 'GET',
+      data: {
+        text: $('#api_test').val()
+      },
+      dataType: 'JSON',
+      success (res) {
+        alert('Sucess: ' + JSON.stringify(res))
+      },
+      error () {
+        alert('Error')
+      }
+    })
+  })
 })

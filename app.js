f4098418138a83165dee6365af9df86c5417fee8
leegeunhyeok <lghlove0509@naver.com>
2019-08-18 17:17:01 +0900

    ADD: uncaughtException 예외 처리 로직 추가

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ const PORT = config.get('port')
 global.__root = __dirname
 require('./src/init')(app)
 
+// 예기치않은 예외 처리
+process.on('uncaughtException', err => {
+  logger.critical(err)
+})
+
 // 서버 실행
 app.listen(PORT, () => logger.info(`Server started, port: ${PORT}`))

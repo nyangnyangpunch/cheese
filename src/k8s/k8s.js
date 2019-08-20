983505dd4de7a12aa1d3b1629daf561a78ce77ed
leegeunhyeok <lghlove0509@naver.com>
2019-08-21 00:07:24 +0900

    ADD: @kubernetes/client-node 라이브러리 추가, 파드 정보 불러오는 로직 구현

---
+++ src/k8s/k8s.js
@@ -0,0 +1,22 @@
+const { logger } = require('../util/logger')
+const k8s = require('@kubernetes/client-node')
+
+const kc = new k8s.KubeConfig()
+kc.loadFromDefault()
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+
+/**
+ * Pods 정보를 불러옵니다.
+ * @param {string} namespace Pods 네임스페이스 (기본: default)
+ */
+const getPods = async (namespace = 'default') => {
+  try {
+    return await k8sApi.listNamespacedPod(namespace).body
+  } catch (e) {
+    logger.error(e)
+    return null
+  }
+}
+
+exports.getPods = getPods

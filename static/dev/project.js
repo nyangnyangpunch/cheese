8bccc3294600a10451fbc8f15d9cd8a6b1493a3d
leegeunhyeok <lghlove0509@naver.com>
2019-08-21 01:10:09 +0900

    UPDATE: 치즈에서 Pods 정보 불러오는 로직 구현

--- static/dev/project.js
+++ static/dev/project.js
@@ -4,22 +4,64 @@
  * Project 메뉴 스크립트
  */
 
-$(function () {
-  // API 테스트
-  $('#send').click(function () {
-    $.ajax({
-      url: '/api/testApi',
-      method: 'GET',
-      data: {
-        text: $('#api_test').val()
-      },
-      dataType: 'JSON',
-      success (res) {
-        alert('Sucess: ' + JSON.stringify(res))
-      },
-      error () {
-        alert('Error')
-      }
+const showPodsList = pods => {
+  if (!pods) {
+    $('#project').html(`
+      <div class="text">프로젝트를 생성하세요! (아직 프로젝트가 존재하지 않습니다)</div>
+      <div class="panel-control">
+        <button class="button white">불러오기</button>
+      </div>
+      <div class="panel-control">
+        <button class="button green">새로 만들기</button>
+      </div>
+    `)
+    return
+  }
+
+  let template = ''
+  pods.forEach(pod => {
+    let containers = ''
+    pod.spec.containers.forEach(container => {
+      containers += `
+        <div class="container">${container.image}</div>
+      `
     })
+
+    template += `
+      <div class="pod">
+        <div class="pod-name">${pod.metadata.name}</div>
+        <div class="pod-namespace">(${pod.metadata.namespace})</div>
+        <div class="host">
+          ${pod.spec.nodeName}<b class="sub">(${pod.status.hostIP})</b> - ${pod.status.phase}
+        </div>
+        <div class="pod-containers">
+          Container(s)
+          ${containers}
+        </div>
+        <div class="start">${pod.status.startTime}</div>
+      </div>
+    `
   })
+
+  $('#project').html(template)
+}
+
+const getPods = () => {
+  $.ajax({
+    url: '/API/getPods',
+    type: 'GET',
+    dataType: 'JSON',
+    success (res) {
+      console.log('getPods: ' + res.apiVersion)
+      showPodsList(res.items)
+    },
+    error (jqXHR, state) {
+      console.error(jqXHR, state)
+      showPodsList()
+    }
+  })
+}
+
+$(function () {
+  getPods()
 })
--- static/js/project.js
+++ static/js/project.js
@@ -5,22 +5,39 @@
  * 
  * Project 메뉴 스크립트
  */
-$(function () {
-  // API 테스트
-  $('#send').click(function () {
-    $.ajax({
-      url: '/api/testApi',
-      method: 'GET',
-      data: {
-        text: $('#api_test').val()
-      },
-      dataType: 'JSON',
-      success: function success(res) {
-        alert('Sucess: ' + JSON.stringify(res));
-      },
-      error: function error() {
-        alert('Error');
-      }
+var showPodsList = function showPodsList(pods) {
+  if (!pods) {
+    $('#project').html("\n      <div class=\"text\">\uD504\uB85C\uC81D\uD2B8\uB97C \uC0DD\uC131\uD558\uC138\uC694! (\uC544\uC9C1 \uD504\uB85C\uC81D\uD2B8\uAC00 \uC874\uC7AC\uD558\uC9C0 \uC54A\uC2B5\uB2C8\uB2E4)</div>\n      <div class=\"panel-control\">\n        <button class=\"button white\">\uBD88\uB7EC\uC624\uAE30</button>\n      </div>\n      <div class=\"panel-control\">\n        <button class=\"button green\">\uC0C8\uB85C \uB9CC\uB4E4\uAE30</button>\n      </div>\n    ");
+    return;
+  }
+
+  var template = '';
+  pods.forEach(function (pod) {
+    var containers = '';
+    pod.spec.containers.forEach(function (container) {
+      containers += "\n        <div class=\"container\">".concat(container.image, "</div>\n      ");
     });
+    template += "\n      <div class=\"pod\">\n        <div class=\"pod-name\">".concat(pod.metadata.name, "</div>\n        <div class=\"pod-namespace\">(").concat(pod.metadata.namespace, ")</div>\n        <div class=\"host\">\n          ").concat(pod.spec.nodeName, "<b class=\"sub\">(").concat(pod.status.hostIP, ")</b> - ").concat(pod.status.phase, "\n        </div>\n        <div class=\"pod-containers\">\n          Container(s)\n          ").concat(containers, "\n        </div>\n        <div class=\"start\">").concat(pod.status.startTime, "</div>\n      </div>\n    ");
   });
+  $('#project').html(template);
+};
+
+var getPods = function getPods() {
+  $.ajax({
+    url: '/API/getPods',
+    type: 'GET',
+    dataType: 'JSON',
+    success: function success(res) {
+      console.log('getPods: ' + res.apiVersion);
+      showPodsList(res.items);
+    },
+    error: function error(jqXHR, state) {
+      console.error(jqXHR, state);
+      showPodsList();
+    }
+  });
+};
+
+$(function () {
+  getPods();
 });

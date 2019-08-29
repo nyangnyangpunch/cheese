365869a5fd4cc07210dcee5d268d0d89398a99bb
yngus4862 <yngus4862@naver.com>
2019-08-29 21:55:06 +0900

    Update setting.js

--- static/js/setting.js
+++ static/js/setting.js
@@ -2,7 +2,62 @@
 
 /**
  * setting.js
- * 
+ *
  * Setting 메뉴 스크립트
  */
-$(function () {});
+$(function () {
+  $('#pod_replica_auto').click(function(){
+    var pod_name = $('#pod_name_auto').val();
+    var namespace = $('#namespace_auto').val();
+    var min = $('#min_auto').val();
+    var max = $('#max_auto').val();
+
+    if(min && max){
+      $.ajax({
+        url: '/API/autoScale',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+          podname: pod_name,
+          namespace: namespace,
+          min: min,
+          max: max
+        },
+        success: function success(res){
+          if(res){
+            location.reload();
+          }
+        },
+        error: function error(jqXHR, state){
+          console.error(jqXHR, state);
+        }
+      });
+    }
+  });
+  $('#pod_replica_scale').click(function(){
+    var pod_name = $('#pod_name_self').val();
+    var namespace = $('#namespace_self').val();
+    var max = $('#max_scale').val();
+
+    if(max){
+      $.ajax({
+        url: '/API/selfScale',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+          podname: pod_name,
+          namespace: namespace,
+          max: max
+        },
+        success: function success(res){
+          if(res){
+            location.reload();
+          }
+        },
+        error: function error(jqXHR, state){
+          console.error(jqXHR, state);
+        }
+      });
+    }
+  });
+});
